e1d1fb35b9f13431fae8455ad7c719c665e2558e
nhoxreddid123 <73044505+nhoxreddid123@users.noreply.github.com>
2021-11-26 17:18:14 +0700

    run neopixel

---
+++ 03_Slave_FW/Core/Src/protocol.c
@@ -0,0 +1,28 @@
+/*
+ * protocol.c
+ *
+ *  Created on: Nov 26, 2021
+ *      Author: hoadv
+ */
+
+#include "protocol.h"
+
+uint8_t encode_msg(driver_t *driver, uint8_t *msg)
+{
+	msg[0] = 0x05;
+	msg[1] = driver->btn;
+	return 1;
+}
+
+/*
+ *		msg: | ADDR | Data[] |
+ */
+uint8_t decode_msg(driver_t *driver, uint8_t *msg, uint8_t len)
+{
+	if (len < sizeof(driver_t))
+	{
+		memcpy(&((uint8_t *)driver)[msg[0]], &msg[1], len - 1);
+		return 1;
+	}
+	return 0;
+}
